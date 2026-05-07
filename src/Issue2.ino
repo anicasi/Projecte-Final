01788e8f22c89626a9325b9006956720ae5b41a8
anicand <alexnicasi2@gmail.com>
2026-05-07 10:23:13 +0200

    Add sensor reading and LED control logic

--- src/Issue2.ino
+++ src/Issue2.ino
@@ -1,1 +1,37 @@
+const int sensorPin = 34;
+const int ledPin = 2;
 
+int sec_max = 300;
+int humit_max = 1500;
+int submergit_max = 4095;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int value = analogRead(sensorPin);
+
+  if (value < 0) value = 0;
+  if (value > 4095) value = 4095;
+
+  Serial.print("Valor brut: ");
+  Serial.print(value);
+  Serial.print("  -->  Estat: ");
+
+  if (value <= sec_max) {
+    Serial.println("Sec");
+    digitalWrite(ledPin, LOW);
+  }
+  else if (value <= humit_max) {
+    Serial.println("Terra humida");
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    Serial.println("Submergit en aigua");
+    digitalWrite(ledPin, HIGH);
+  }
+
+  delay(3000);
+}
